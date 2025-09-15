8a7190653d32a55f6442f68945bb0fea8f09d2fe
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-09-15 20:45:43 +0800

    corrected error of blank published page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife', // Change from 'es' to 'iife'
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js'
+      }
+    }
+  }
 })
